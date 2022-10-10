2f545b4a4b387e196dc149eabb163857cf9d7897
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-10-10 10:41:47 -0300

    Att 10-10

---
+++ src/AppStyle.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContainerHomePage = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Ubuntu', sans-serif;
+    color: white;
+`
---
+++ src/config/globalStyles.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const globalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-shadow: 0;
+        background: #010015;
+    }
+`
+
+export default globalStyle;
